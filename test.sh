171172c786dac44a4d2291647097e1545976235b
Joshua Ji <joshua.ji@jiwhiz.com>
2023-09-15 02:48:41 +0000

    update README

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/Applications/CLion.app/Contents/bin/ninja/mac/ninja -G Ninja -S /Users/joshuaji/code/skewl/cmput415/generator-joshuanianji -B /Users/joshuaji/code/skewl/cmput415/generator-joshuanianji/cmake-build-debug
 
-docker run -it \
-    -v "$(pwd)":/app test \
+docker run -it -v "$(pwd)":/app test \
     bash -c "cd /demo && make"
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S /app/generator-joshuanianji -B /app/generator-joshuanianji/cmake-build-debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S /app/tests/generator-joshuanianji -B /app/tests/generator-joshuanianji/cmake-build-debug
 
-cmake --build /app/generator-joshuanianji/cmake-build-debug --target all -j 8
+cmake --build /app/tests/generator-joshuanianji/cmake-build-debug --target all -j 8
