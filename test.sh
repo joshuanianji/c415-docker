d60d9fe16548bf25e3dc5247cd9e0882f911d9be
Joshua Ji <joshua.ji@jiwhiz.com>
2023-09-15 06:46:00 +0000

    Discard tmp files

--- test.sh
+++
@@ -1,8 +0,0 @@
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=/Applications/CLion.app/Contents/bin/ninja/mac/ninja -G Ninja -S /Users/joshuaji/code/skewl/cmput415/generator-joshuanianji -B /Users/joshuaji/code/skewl/cmput415/generator-joshuanianji/cmake-build-debug
-
-docker run -it -v "$(pwd)":/app test \
-    bash -c "cd /demo && make"
-
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S /app/tests/generator-joshuanianji -B /app/tests/generator-joshuanianji/cmake-build-debug
-
-cmake --build /app/tests/generator-joshuanianji/cmake-build-debug --target all -j 8
